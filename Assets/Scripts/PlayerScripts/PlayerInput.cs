740ba593e31ad66c804b77dc4415d40f7a8d1a2e
Elderly <elderlyinside@gmail.com>
2022-12-10 23:13:37 +0300

    Реализован "реилган"

--- Assets/Scripts/PlayerScripts/PlayerInput.cs
+++ Assets/Scripts/PlayerScripts/PlayerInput.cs
@@ -6,15 +6,32 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     public bool IsFire { get; private set; }
+    public bool IsPressedFire { get; private set; }
+    public bool NotPressedFire { get; private set; }
 
     public event Action Fired = default;
+    public event Action PressedFire = default;
+    public event Action OffPressedFire = default;
     void Update()
     {
         IsFire = Input.GetKey(KeyCode.Mouse0);
-
         if (IsFire && Fired != null)
         {
             Fired();
         }
+
+        IsPressedFire = Input.GetKey(KeyCode.Mouse0);
+        if (IsPressedFire && PressedFire != null)
+        {
+            PressedFire();
+        }
+
+        NotPressedFire = Input.GetKeyUp(KeyCode.Mouse0);
+        if (NotPressedFire && OffPressedFire != null)
+        {
+            OffPressedFire();
+        }
+
+
     }
 }
---
+++ Assets/Scripts/WeaponScripts/ChargaredGunShot.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargaredGunShot : MonoBehaviour
+{
+    Camera _fpsCam;
+    [SerializeField] float _chargaredTime;
+    [SerializeField] float _maxCharge;
+    [SerializeField] bool _chargared;
+    [SerializeField] int _damage;
+    [SerializeField] float _range;
+    [SerializeField] FlashScript _flashScript;
+    [SerializeField] ShotImpactScript _shotImpactScript;
+    [SerializeField] float _delayShot;
+    [SerializeField] bool _readyShot;
+    PlayerInput _playerInputScript;
+    void Start()
+    {
+        _fpsCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        _flashScript = gameObject.GetComponent<FlashScript>();
+        _shotImpactScript = gameObject.GetComponent<ShotImpactScript>();
+    }
+
+    private void OnEnable()
+    {
+        _playerInputScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInput>();
+        _playerInputScript.PressedFire += OnPressedFired;
+        _playerInputScript.OffPressedFire += NotPressedFired;
+    }
+
+    private void OnDisable()
+    {
+        _playerInputScript.PressedFire -= OnPressedFired;
+        _playerInputScript.OffPressedFire -= NotPressedFired;
+    }
+
+    void OnPressedFired()
+    {
+        if (_readyShot == true)
+        {
+            _chargaredTime += 2 * Time.deltaTime;
+            if (_chargaredTime > _maxCharge)
+            {
+                NotPressedFired();
+                _chargaredTime = 0f;
+                _readyShot = false;
+                StartCoroutine(SwitcherReadyShot());
+            }
+        }  
+    }
+
+    void NotPressedFired()
+    {
+        if (_readyShot == true)
+        {
+            int _chargaredDamage = _damage + (_damage * (int)_chargaredTime);
+            _chargaredTime = 0f;
+            RaycastHit hit;
+            _flashScript.PlayFlash();
+            if (Physics.Raycast(_fpsCam.transform.position, _fpsCam.transform.forward, out hit, _range))
+            {
+                Debug.Log(hit.transform.name);
+                EnemyHealt eh = hit.transform.GetComponent<EnemyHealt>();
+                if (eh != null)
+                {
+                    eh.TakeDamage(_chargaredDamage);
+                }
+                _shotImpactScript.SpawnImpact(hit.point, Quaternion.LookRotation(hit.normal));
+            }
+            _readyShot = false;
+            StartCoroutine(SwitcherReadyShot());
+        }
+    }
+
+    IEnumerator SwitcherReadyShot()
+    {
+        yield return new WaitForSeconds(_delayShot);
+        _readyShot = true;
+    }
+}
